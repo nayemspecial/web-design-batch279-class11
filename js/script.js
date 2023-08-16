46aba7c03f7415e88dd9f2ca3badb5b73762b4fa
Nayemur Rahman <nayemspecial@gmail.com>
2023-08-16 23:16:24 +0600

    nivo Slider customization

---
+++ js/script.js
@@ -0,0 +1,28 @@
+        /*
+        document.write("This is JavaScript Output");
+
+        window.alert("This is JavaScript Output");
+
+        document.getElementById("heading").innerHTML = 255;
+
+        console.log("This is JavaScript output. that is printed in console");
+        
+        var x = 3;
+        var y = 6;
+        var z = x+y;
+
+        document.write("Summation of " +x+ " and " +y+ " is ", z);
+
+        
+        Arithmatic +, -, *, /, ++, --, %
+
+        Assignment =, +=, -=, *=, /=, %=
+
+        integer 
+        float 
+        boolean
+        string 
+        array 
+        Object 
+        null 
+        */
